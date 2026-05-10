32c06e4f508fde6f6f40fe912c30d998fa1edf21
$H!NDGEKYUME <cto@whatsinlab.com>
2026-05-10 22:25:51 +0800

    Add macOS platform support

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "Livable",
     platforms: [
         .iOS(.v17),
+        .macOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
